572aae2b3bc73ee275ac697eba9526f9719b9e7a
muditmathurr <muditmathur05@gmail.com>
2024-07-05 12:28:48 +0530

    created a custom method or a custom hook and call that hook in App.jsx

--- Apis/frontend/src/App.jsx
+++ Apis/frontend/src/App.jsx
@@ -4,36 +4,15 @@ import axios from "axios";
 
 function App() {
 
-  const [products, setProducts] = useState([]);
-  const [error,setError] = useState(false)
-  const [loading, setLoading] = useState(false)
+  const [products, error, loading] = customReactQuery("/api/products");
 
-
-  useEffect(() => {
-    (async () => {
-      try {
-        setLoading(true)
-        setError(false)
-        const response = await axios.get("/api/products");
-        console.log(response.data);
-        setProducts(response.data)
-        setLoading(false)
-      } catch (error) {
-        setError(true)
-        setLoading(false)
-      }
-    })();
-  }, []);
-
-
-  if(error){
-    return <h1>Something went wrong</h1>
+  if (error) {
+    return <h1>Something went wrong</h1>;
   }
 
-  if(loading) {
-    return <h1>Loading ...</h1>
+  if (loading) {
+    return <h1>Loading ...</h1>;
   }
-
 
   return (
     <>
@@ -44,3 +23,26 @@ function App() {
 }
 
 export default App;
+
+const customReactQuery = (urlPath) => {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        setError(false);
+        const response = await axios.get(urlPath);
+        console.log(response.data);
+        setProducts(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    })();
+  }, []);
+  return [products, error, loading];
+};
