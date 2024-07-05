1d150ea6f60d913d746d329136d2e824a7864baf
muditmathurr <muditmathur05@gmail.com>
2024-07-05 12:23:01 +0530

    handle edge cases if url is wrong and loading.. screen while data is loading

--- Apis/frontend/src/App.jsx
+++ Apis/frontend/src/App.jsx
@@ -3,15 +3,37 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
+
   const [products, setProducts] = useState([]);
+  const [error,setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+
 
   useEffect(() => {
     (async () => {
-      const response = await axios.get("/api/products");
-      console.log(response.data);
-      setProducts(response.data)
+      try {
+        setLoading(true)
+        setError(false)
+        const response = await axios.get("/api/products");
+        console.log(response.data);
+        setProducts(response.data)
+        setLoading(false)
+      } catch (error) {
+        setError(true)
+        setLoading(false)
+      }
     })();
   }, []);
+
+
+  if(error){
+    return <h1>Something went wrong</h1>
+  }
+
+  if(loading) {
+    return <h1>Loading ...</h1>
+  }
+
 
   return (
     <>
